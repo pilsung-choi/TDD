c0ad7d63431c3ed7fceb66dabe8bdcd674c319d3
pilsung-choi <vlftjd159753@gmail.com>
2022-12-01 19:18:41 +0900

    calculate

---
+++ src/test/calculator.test.js
@@ -0,0 +1,55 @@
+const Calculator = require('../calculator.js');
+
+describe('Calculator', () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+  it('inits with 0', () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it('sets', () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  });
+
+  it('clear', () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it('add', () => {
+    cal.set(1);
+    cal.add(2);
+    expect(cal.value).toBe(3);
+  });
+
+  it('substracts', () => {
+    cal.subtract(1);
+    expect(cal.value).toBe(-1);
+  });
+
+  it('multiplies', () => {
+    cal.set(5);
+    cal.multiply(4);
+    expect(cal.value).toBe(20);
+  });
+  describe('divide', () => {
+    it('0 / 0 === NaN', () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+    it('1 / 0 === Infinity', () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+    it('4 / 4 === 1', () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
+  });
+});
